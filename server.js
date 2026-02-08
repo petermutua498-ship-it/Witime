da8697d60062fddbc846535aba5534e80eefea37
Peter Mutua <petermutua498@gmail.com>
2026-02-08 14:20:25 +0300

    Fix Reder port bindig

--- server.js
+++ server.js
@@ -148,4 +148,8 @@ function initiateIntaSendPayment(phone, amount) {
   });
 }
 
-app.listen(3000, () => console.log('WiTime running on port 3000'));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log('WiTime running on port 3000');
+});
