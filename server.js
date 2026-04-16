bc6bf4f089edec26c6e250b76a56ec19f50296a3
peter mutua <petermutua498@gmail.com>
2026-04-16 16:36:32 +0300

    fix server.js

--- server.js
+++ server.js
@@ -281,8 +281,8 @@ app.post("/admin-login", (req, res) => {
     const { username, password } = req.body;
 
     if(
-        username === process.env.ADMIN_USER && 
-        password === process.env.ADMIN_PASS
+        username === "admin" && 
+        password === "1234"
     ) {
         res.json({ status: "ok" });
     } else {
